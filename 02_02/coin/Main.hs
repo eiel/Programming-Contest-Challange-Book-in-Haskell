6dc44be718fffaab5e40b8c48a2c7c6467aa1d69
furu <ba09219@gmail.com>
2012-03-14 23:20:02 +0900

    foldrで書き直した

--- 02_02/coin/Main.hs
+++ 02_02/coin/Main.hs
@@ -7,21 +7,19 @@ import Text.Printf
 main = do
   cs <- forM [1..6] $ \_ -> getInt
   a <- getInt
-  print cs
-  print a
   print $ solve cs a
     where
       getInt :: IO Int
       getInt = fmap read $ getLine
 
 solve :: [Int] -> Int -> Int
-solve cs a = solve' (reverse $ zip cs vs) a 
+solve cs a = snd $ foldr solve' (a,0) $ zip cs vs
   where
-    solve' :: [(Int, Int)] -> Int -> Int
-    solve' [] _ = 0
-    solve' ((c,v):vcs) a' = t + solve' vcs (a' - (t * v))
+    solve' :: (Int, Int) -> (Int, Int) -> (Int, Int)
+    solve' (c,v) (a',t') = (a' - (t * v), t'+t)
       where
         t = min (a' `div` v) c
 
 vs = [1, 5, 10, 50, 100, 500]
 
+solve01 = solve [3, 2, 1, 3, 0, 2] 620
