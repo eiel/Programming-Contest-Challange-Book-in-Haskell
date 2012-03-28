58160b29a8d549615069a97165bec23784a18766
furu <ba09219@gmail.com>
2012-03-28 22:36:09 +0900

    ソートできるようにした

--- 02_02/interval_scheduling/Main.hs
+++ 02_02/interval_scheduling/Main.hs
@@ -1,24 +1,23 @@
 module Main where
 
 import Control.Monad (forM)
+import Data.List (sort)
 
 main = do
   n <- getInt
   ss <- forM [1..n] $ \n -> getInt
   ts <- forM [1..n] $ \n -> getInt
---  print ss
---  print ts
-  print $ solve ss ts 0 0
+  print $ solve (sort' ss ts) 0 0
     where
       getInt :: IO Int
       getInt = fmap read $ getLine
 
 -- ルール：選べる仕事の中で、終了時間が最も早いものを選ぶことを繰り返す
-solve :: [Int] -> [Int] -> Int -> Int -> Int
-solve [] [] ans end = ans
---solve (s:ss) (t:ts) ans end = if end < s then solve ss ts (ans + 1) t
---                              else solve ss ts ans end
-solve (s:ss) (t:ts) ans end | end < s = solve ss ts (ans + 1) t
-                            | otherwise = solve ss ts ans end
+solve :: [(Int, Int)] -> Int -> Int -> Int
+solve [] ans end = ans
+solve ((t, s):ss) ans end | end < s = solve ss (ans + 1) t
+                                | otherwise = solve ss ans end
+sort' :: [Int] -> [Int] -> [(Int, Int)]
+sort' ss ts = sort $ zip ts ss
 
-solve' = solve [1, 2, 4, 6, 8] [3, 5, 7, 9, 10] 0 0
+solve' = solve (sort' [2, 1, 4, 6, 8] [5, 3, 7, 9, 10]) 0 0
